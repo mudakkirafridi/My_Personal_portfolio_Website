1f5e4de16f5ccc67ecbdb97a568556133b84b825
Mudakir <mudakkirafridi@gmail.com>
2023-07-26 10:37:52 +0500

    hi

---
+++ javasc.js
@@ -0,0 +1,8 @@
+
+ var typed = new Typed('.autotypes', {
+    strings: ["Web Development..","App development..","Cross-Platform..","Development.."],
+    typeSpeed: 30,
+    backSpeed:30,
+    loop: true
+ });
+
